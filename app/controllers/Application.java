88cec4b7b7988eedc19aa915451b68df6ce0a372
gwz <gwz@gwz-laptop>
2014-09-10 11:32:30 +0800

    登录

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,30 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import japidviews._javatags.CommonUtils;
 
 import java.util.*;
 
 import models.*;
 
-public class Application extends Controller {
+public class Application extends BaseController {
 
-    public static void index() {
-//        render();
-    }
+	public static void loginPage() {
+		Person p = CommonUtils.getCurrentPerson();
+		if (null != p) {
+			AdminController.flashPurchase(0l);
+		}
+		renderJapid();
+	}
 
+	public static void loginFromWebsite(String loginName, String password) {
+		Person loginedPerson = Person.login(loginName, password);
+		if (null != loginedPerson) {
+			session.put(CURRENT_PERSON_ID, loginedPerson.id);
+			loginedPerson.updateLoginTime();
+			AdminController.flashPurchase(0l);
+		} else {
+			renderText("error login name or password");
+		}
+	}
 }
